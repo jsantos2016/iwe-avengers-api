753e6b365a74350afd0fba6dc4be2093cf9442e7
Noturno <Noturno@DESKTOP-NQ4RTT7>
2018-11-26 22:25:26 -0200

    #Modulo 3

--- iwe-avengers-integration-tests/src/test/java/com/iwe/avengers/test/avengers.feature
+++ iwe-avengers-integration-tests/src/test/java/com/iwe/avengers/test/avengers.feature
@@ -3,20 +3,26 @@ Feature: Perform integrated tests on the Avengers registration API
 Background:
 * url 'https://tpgj5f4zj2.execute-api.us-east-1.amazonaws.com/dev'
 
+Scenario: Should return not found Avenger
+
+Given path 'avengers','not-found-id'
+When method get
+Then status 404
+
 Scenario: Get Avenger by Id
 
-Given path 'avengers','aaaa-bbb-cccc-dddd'
+Given path 'avengers','aaaa-bbbb-cccc-dddd'
 When method get
 Then status 200
 And match response == {id: '#string', name: 'Iron Man', secretIdentity: 'Tony Stark' }
 
-Scenario: Create Avenger
-
-Given path 'avengers'
-And request {name: 'Iron Man', secretIdentity: 'Tony Stark'}
-When method post
-Then status 201
-And match response == {id: '#string', name: 'Iron Man', secretIdentity: 'Tony Stark' }
+#Scenario: Create Avenger
+#
+#Given path 'avengers'
+#And request {name: 'Iron Man', secretIdentity: 'Tony Stark'}
+#When method post
+#Then status 201
+#And match response == {id: '#string', name: 'Iron Man', secretIdentity: 'Tony Stark' }
 
 Scenario: Must return 400 for invalid creation payload
 
--- iwe-avengers-search/src/main/java/com/iwe/avengers/SearchAvengersHandler.java
+++ iwe-avengers-search/src/main/java/com/iwe/avengers/SearchAvengersHandler.java
@@ -1,14 +1,36 @@
 package com.iwe.avengers;
 
+import java.util.Optional;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.iwe.avenger.dao.AvengerDAO;
+import com.iwe.avengers.exception.AvengerNotFoundException;
 import com.iwe.avenger.dynamodb.entity.Avenger;
 import com.iwe.avenger.lambda.response.HandlerResponse;
 
 public class SearchAvengersHandler implements RequestHandler<Avenger, HandlerResponse> {
 
+	private AvengerDAO dao = new AvengerDAO();
+
 	@Override
-	public HandlerResponse handleRequest(final Avenger avenger, final Context context) {
+	public HandlerResponse handleRequest( final Avenger avenger, final Context context ){
+
+		final String id = avenger.getId();
+		
+		context.getLogger().log( "[#] - Searching Avenger by id: " + id);
+		
+		final Optional<Avenger> avengerRetieved = dao.search( id );
+		
+		if( avengerRetieved.isPresent() ){
+			context.getLogger().log( "[#] - Avenger found " + avengerRetieved.get().getName() );
+			
+			
+			return HandlerResponse.builder().setObjectBody( avengerRetieved.get() ).build();
+		}
+
 		return null;
+		//throw new AvengerNotFoundException( "[NotFound] - Avenger id: " );
+
 	}
 }
